7de9ba572ebbc8907655c8b2e804cb0704c02962
ganeshgit <gadadeganesh27@gmail.com>
2026-09-25 00:01:41 +0530

    Initial commit from Create Next App

---
+++ next.config.ts
@@ -0,0 +1,22 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.dummyjson.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dummyjson.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
